2b5129d0ad3d3704dee262bd15772155f92a4b39
Vasily Begunov <vibegunov@gmail.com>
2025-09-01 20:21:15 +0300

    feat(logger): add zap logger; add HTTP logger middleware

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,22 +6,31 @@ import (
 
 	appenv "github.com/funkymotions/go-ya-practicum-metrics/internal/config/env"
 	"github.com/funkymotions/go-ya-practicum-metrics/internal/handler"
+	"github.com/funkymotions/go-ya-practicum-metrics/internal/logger"
+	"github.com/funkymotions/go-ya-practicum-metrics/internal/middleware"
 	"github.com/funkymotions/go-ya-practicum-metrics/internal/repository"
 	"github.com/funkymotions/go-ya-practicum-metrics/internal/service"
 	"github.com/go-chi/chi"
+	"go.uber.org/zap"
 )
 
 type Server struct {
 	server *http.Server
+	logger *zap.Logger
 }
 
 func (s *Server) Run() error {
-	log.Printf("Starting server on %s\n", s.server.Addr)
+	s.logger.Info("Starting server", zap.String("addr", s.server.Addr))
 	return s.server.ListenAndServe()
 }
 
 func NewServer(v *appenv.Variables) *Server {
 	var apiPrefix = "/update"
+	// logger
+	logger, err := logger.NewLogger(zap.NewAtomicLevelAt(zap.InfoLevel))
+	if err != nil {
+		log.Fatalf("failed to initialize logger: %v", err)
+	}
 	// repositories
 	metricRepo := repository.NewMetricRepository()
 	// services
@@ -30,6 +39,7 @@ func NewServer(v *appenv.Variables) *Server {
 	metricHandler := handler.NewMetricHandler(metricService)
 	// routing
 	r := chi.NewRouter()
+	r.Use(middleware.HTTPLogMiddleware(logger))
 	r.Get("/", http.HandlerFunc(metricHandler.GetAllMetrics))
 	r.Get("/value/{type}/{name}", http.HandlerFunc(metricHandler.GetMetric))
 	r.Post(apiPrefix+"/{type}/{name}/{value}", http.HandlerFunc(metricHandler.SetMetric))
@@ -39,5 +49,6 @@ func NewServer(v *appenv.Variables) *Server {
 	}
 	return &Server{
 		server: server,
+		logger: logger,
 	}
 }
